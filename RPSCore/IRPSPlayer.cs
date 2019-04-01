270457be74f9206371133923baf12121691a1034
lovells4 <49151861+lovells4@users.noreply.github.com>
2019-04-01 13:40:00 +0100

    Add files via upload

--- RPSCore/IRPSPlayer.cs
+++ RPSCore/IRPSPlayer.cs
@@ -1,11 +1,17 @@
 ﻿namespace RPSCore
 {
+    // For a match the sequence will follow the pattern
+    // GetReady(); to set parameters for the match such as how many games will be played
+    // Repeated calls to the next two methods for each game
+    //  MakeMove(); your turn
+    //  GameResult(); result of the game included what my move was
+    // End of repeated calls
+    // Result(); to give final result for match
     public interface IRPSPlayer
     {
-        //get ready to play numGames games and in each game you will have numWaterbombs waterbombs
-        //and numDynamite dynamites
+        //get ready to play numGames games and in each game you will have numDynamite dynamites
         //return any comments you want to make to your opponent
-        string GetReady(int numGames, int numWaterbombs, int numDynamite);
+        string GetReady(int numGames, int numDynamite);
 
         //return the move you want to make for this game
         Move MakeMove();
